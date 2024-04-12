788dd163a958eecd6f26493a2b1d613764bfe266
Ramsensei <ramsensei@estudiantec.cr>
2024-04-11 21:48:53 -0600

    Hot_bit six

--- Hot_Bit.sv
+++ Hot_Bit.sv
@@ -4,10 +4,11 @@ module Hot_Bit #(DEPTH = 8) (index, Out);
     input [$clog2(DEPTH)-1:0] index;
     output [DEPTH-1:0] Out;
 
+
     genvar i;
     generate
         for (i = 0; i < DEPTH; i++) begin : generate_hotbit_outputs
-            Nbit_Equal_Comp #(BITS) equal_n_bits(.Data0(index), i, .Out(Out[i]));
+            Nbit_Equal_Comp #(BITS) equal_n_bits(.Data0(index), .Data1(i), .Out(Out[i]));
         end
     endgenerate
     
