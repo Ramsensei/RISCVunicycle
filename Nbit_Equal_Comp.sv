4c0548a022d5507510c0b4c10782d4e0d7efc488
Ramsensei <ramsensei@estudiantec.cr>
2024-04-11 22:17:02 -0600

    Nbit fix

--- Nbit_Equal_Comp.sv
+++ Nbit_Equal_Comp.sv
@@ -1,13 +1,17 @@
 `include "Equal_Comp.sv"
 module Nbit_Equal_Comp #(BITS = 32) (Data0, Data1, Out);
     input [BITS-1:0] Data0, Data1;
-    output [BITS-1:0] Out;
+    output reg Out;
+
+    reg [BITS-1:0] Comps;
 
     genvar i;
     generate
         for (i = 0; i < BITS; i++) begin : generate_NComps
-            Equal_Comp equal_one_bit(.Data0(Data0[i]), .Data1(Data1[i]), .Out(Out[i]));
+            Equal_Comp equal_one_bit(.Data0(Data0[i]), .Data1(Data1[i]), .Out(Comps[i]));
         end
     endgenerate
     
+    assign Out = &Comps;
+
 endmodule
